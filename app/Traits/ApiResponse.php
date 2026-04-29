b72bb547d000063ba2f1b7531bc6de5ecdb7300a
rifkialansyarii <alansyari3431@gmail.com>
2026-04-29 23:39:44 +0800

    feat: create policy for employee - create custom exception for policy exception - update trait api response template

--- app/Traits/ApiResponse.php
+++ app/Traits/ApiResponse.php
@@ -6,16 +6,18 @@ use Illuminate\Http\JsonResponse;
 
 trait ApiResponse
 {
-    protected function sendSuccess($data, string $message = null, int $code = 200): JsonResponse {
+    protected function sendSuccess($data, string $message = null, int $code = 200): JsonResponse
+    {
         return response()->json([
             'success' => true,
             'code' => $code,
             'message' => $message,
-            'data'    => $data, 
+            'data' => $data,
         ], $code);
     }
 
-    protected function sendError(string $message, int $code = 400, ?array $errors = null): JsonResponse {
+    protected function sendError(string $message, int $code = 400, ?array $errors = null): JsonResponse
+    {
         return response()->json([
             'success' => false,
             'code' => $code,
