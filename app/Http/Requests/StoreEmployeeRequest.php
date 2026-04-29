4316c98c955015ea85023bbc32f7a88724d930ad
rifkialansyarii <alansyari3431@gmail.com>
2026-04-29 21:53:39 +0800

    refactor: create custom error validation message - UpdateEmployeeRequest has custom message unauthorized

--- app/Http/Requests/StoreEmployeeRequest.php
+++ app/Http/Requests/StoreEmployeeRequest.php
@@ -3,12 +3,16 @@
 namespace App\Http\Requests;
 
 use App\Enums\Gender;
+use App\Traits\ApiResponse;
 use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Validation\Rule;
 
 class StoreEmployeeRequest extends FormRequest
 {
+    use ApiResponse;
     /**
      * Determine if the user is authorized to make this request.
      */
@@ -42,11 +46,21 @@ class StoreEmployeeRequest extends FormRequest
             'birth_date' => [Rule::date()->format('Y-m-d')],
             'gender' => [Rule::enum(Gender::class)],
             'phone_number' => 'string|min:10|max:15|unique:App\Models\Employee,phone_number',
-            'avatar' => 'string|max:255',
             'village_code' => 'string|size:10',
             'district_code' => 'string|size:6',
             'city_code' => 'string|size:4',
             'province_code' => 'string|size:2',
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            $this->sendError(
+                message: "The given data was invalid",
+                code: 422,
+                errors: $validator->errors()->toArray()
+            )
+        );
     }
 }
