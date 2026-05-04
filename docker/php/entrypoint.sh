787be0b87f65eb5504da6a25f772b04cd4183547
rifkialansyarii <alansyari3431@gmail.com>
2026-05-04 18:26:46 +0800

    chore (docker): setup docker environment and configuration - Added Docker configuration for Laravel application - Created Dockerfiles for PHP and Nginx - Added setup script to initialize the environment and run migrations

---
+++ docker/php/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir -p /var/www/html/storage/framework/cache/data \
+         /var/www/html/storage/framework/views \
+         /var/www/html/storage/framework/sessions \
+         /var/www/html/bootstrap/cache
+
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+
+find /var/www/html/storage -type d -exec chmod 775 {} \;
+find /var/www/html/storage -type f -exec chmod 664 {} \;
+find /var/www/html/bootstrap/cache -type d -exec chmod 775 {} \;
+find /var/www/html/bootstrap/cache -type f -exec chmod 664 {} \;
+
+# 4. Lanjutkan menjalankan command utama (php-fpm)
+exec "$@"
