9eb8f288cb2930a029f4bc9e6e141980f1df4668
¹.cÐ <root@vmi3284472.contaboserver.net>
2026-05-11 13:53:24 +0200

    build: update prod_setup script - update prod_setup script to support issue SSL certificate   for SSL - Add two config file nginx, base config and https config

--- scripts/prod_setup.sh
+++ scripts/prod_setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+DOMAIN="domain.my.id"
+EMAIL="john@doe.com"
+DATA_PATH="data"
+NGINX_CONF_DIR="docker/nginx"
 
 echo "Setup laravel docker production environment"
 
@@ -7,37 +11,52 @@ if ! docker info > /dev/null 2>&1; then
     exit 1
 fi
 
+#######################
+# SETUP ENV
+#######################
 if [ ! -f .env ]; then
     echo "Copying environment file..."
     cp .env.example .env
     sed -i 's/# DB_DATABASE=laravel/DB_DATABASE=app/' .env
     sed -i 's/# DB_USERNAME=root/DB_USERNAME=laravel/' .env
     sed -i 's/# DB_PASSWORD=/DB_PASSWORD=secret/' .env
-
-    set -o allexport
-    source .env
-    set +o allexport
-
-    read -p "Input your email for issue SSL certificate:  " email
-
-    export EMAIL=$email
 else
     echo ".env file already exists. Skipping copy."
 fi
 
-echo "Building Docker containers..."
-docker compose -f docker-compose.prod.yml build --no-cache
-
+##########################
+# ISSUE CERTIFICATE FOR SSL
+###########################
 echo "Issue Certificate for SSL..."
-docker compose run --rm certbot certonly \ 
-    --webroot -w /var/www/certbot \ 
-    -d api-pegawai-4a.akufarish.my.id \
-    --email $EMAIL \
-    --agree-tos \
-    --no-eff-email
+cp $NGINX_CONF_DIR/default.base.conf $NGINX_CONF_DIR/default.prod.conf
 
+docker compose -f docker-compose.prod.yml up nginx -d
+
+sleep 3
+
+docker compose -f docker-compose.prod.yml run --rm certbot certonly --webroot \
+    --webroot-path=/var/www/certbot \
+    --email $EMAIL \
+    -d $DOMAIN \
+    --rsa-key-size 4096 \
+    --agree-tos \
+    --non-interactive
+
+if [ -d "$DATA_PATH/live/$DOMAIN" ]; then
+    echo "execute HTTPS Config Nginx"
+    cp $NGINX_CONF_DIR/default.https.conf $NGINX_CONF_DIR/default.prod.conf
+else
+    echo "[ERROR] Failed to issue certificate"
+    exit 1
+fi
+
+#######################
+# START ALL CONTAINERS
+#######################
 echo "Starting Docker containers..."
 docker compose -f docker-compose.prod.yml up -d
+
+docker compose -f docker-compose.prod.yml exec nginx nginx -s reload
 
 echo -n "Waiting for services to initialize."
 until docker compose -f docker-compose.prod.yml exec -T mysql mysqladmin ping -h"127.0.0.1" --silent; do
@@ -46,17 +65,19 @@ until docker compose -f docker-compose.prod.yml exec -T mysql mysqladmin ping -h
 done
 echo -e "\nDatabase is ready to accept connections!"
 
+
+###########################################
+# SETUP LARAVEL APP KEY, MIGRATION, SEEDERS
+###########################################
 if ! grep -q "APP_KEY=base64:" .env; then
     echo "Generating application key..."
     docker compose -f docker-compose.prod.yml exec php php artisan key:generate
 fi
 
-# Run migrations and seeders
 echo "Running database migrations and seeders..."
 docker compose -f docker-compose.prod.yml exec php php artisan migrate:fresh --force
 docker compose -f docker-compose.prod.yml exec php php artisan db:seed
 
-# Clear and cache configs
 echo "Optimizing Laravel..."
 docker compose -f docker-compose.prod.yml exec php php artisan config:clear
 docker compose -f docker-compose.prod.yml exec php php artisan cache:clear
