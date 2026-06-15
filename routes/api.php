3bfcf70696f50b59f91f9b76b7c0e7950774d430
alansyari3431@gmail.com <root@vmi3284472.contaboserver.net>
2026-06-15 09:26:51 +0800

    add basic security, rate limiting and anti xss

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ use App\Http\Controllers\VillageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 Route::middleware(['auth:auth-jwt', 'throttle:60,1'])->group(function () {
 
     Route::get('/employees', [EmployeeController::class, 'index'])->middleware('can:viewAny, App\Models\Employee');
