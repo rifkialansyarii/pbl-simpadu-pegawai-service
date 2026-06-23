2449db5ed207ba504d4818220442ce382cc315f5
rifkialansyarii <alansyari3431@gmail.com>
2026-06-23 18:48:37 +0800

    feat: fix logic flaw session can open when date has not started

--- app/Services/ClassSessionService.php
+++ app/Services/ClassSessionService.php
@@ -14,15 +14,17 @@ final class ClassSessionService
 {
     public function __construct(
         private ClassSessionRepositoryInterface $repository,
-    ) {
-    }
+    ) {}
 
     public function getAllClassSessions(User $user, array $filters = [])
     {
-        if ($user->role === 'admin-pegawai' || $user->role === 'super-admin') {
+        if ($user->role === "admin-pegawai" || $user->role === "super-admin") {
             return $this->repository->getAll($filters);
-        } else if ($user->role === 'dosen') {
-            return $this->repository->getAllByLecturer($user->detail_id, $filters);
+        } elseif ($user->role === "dosen") {
+            return $this->repository->getAllByLecturer(
+                $user->detail_id,
+                $filters,
+            );
         } else {
             return $this->repository->getAllByClass($user->class_id, $filters);
         }
@@ -45,77 +47,85 @@ final class ClassSessionService
 
     public function generateClassSession(array $attributes)
     {
-        $data = array();
+        $data = [];
 
         $sessionAmount = 16;
 
-        $sessionDate = $attributes['start_date'];
+        $sessionDate = $attributes["start_date"];
 
         for ($i = 0; $i < $sessionAmount; $i++) {
-
             array_push($data, [
-                "pengampu_id" => $attributes['pengampu_id'],
-                "lecturer_id" => $attributes['lecturer_id'],
-                'session_number' => $i + 1,
-                "class_id" => $attributes['class_id'],
-                "class_name" => $attributes['class_name'],
-                "course_code" => $attributes['course_code'],
-                "course_name" => $attributes['course_name'],
+                "pengampu_id" => $attributes["pengampu_id"],
+                "lecturer_id" => $attributes["lecturer_id"],
+                "session_number" => $i + 1,
+                "class_id" => $attributes["class_id"],
+                "class_name" => $attributes["class_name"],
+                "course_code" => $attributes["course_code"],
+                "course_name" => $attributes["course_name"],
                 "session_date" => $sessionDate,
-                "start_time" => $attributes['start_time'],
-                "end_time" => $attributes['end_time'],
+                "start_time" => $attributes["start_time"],
+                "end_time" => $attributes["end_time"],
             ]);
 
-            $sessionDate = Carbon::parse($sessionDate)->addDays(7)->toDateString();
+            $sessionDate = Carbon::parse($sessionDate)
+                ->addDays(7)
+                ->toDateString();
         }
 
-        return DB::transaction(function () use ($data, $sessionAmount, $attributes) {
-
-            $totalData = DB::table('class_sessions')
-                ->where('class_name', $attributes['class_name'])
-                ->where('course_name', $attributes['course_name'])
+        return DB::transaction(function () use (
+            $data,
+            $sessionAmount,
+            $attributes,
+        ) {
+            $totalData = DB::table("class_sessions")
+                ->where("class_name", $attributes["class_name"])
+                ->where("course_name", $attributes["course_name"])
                 ->lockForUpdate()
                 ->count();
 
             if ($totalData >= 16) {
                 throw new Exception("class sessions have been added");
-
             }
 
             return $this->repository->generate($data, $sessionAmount);
         });
     }
 
-    public function updateClassSession(array $attributes, ClassSession $classSession)
-    {
-        if ($classSession->is_already_opened) {
-            throw new Exception("class session has been opened");
-
-        }
+    public function updateClassSession(
+        array $attributes,
+        ClassSession $classSession,
+    ) {
         return $this->repository->update($classSession, $attributes);
     }
-
 
     public function deleteClassSession(array $attributes)
     {
         return DB::transaction(function () use ($attributes) {
             return $this->repository->bulkDelete($attributes);
         });
-
     }
 
-    public function addSessionMaterial(ClassSession $classSession, array $attributes)
-    {
+    public function addSessionMaterial(
+        ClassSession $classSession,
+        array $attributes,
+    ) {
         return DB::transaction(function () use ($classSession, $attributes) {
-            return $this->repository->createSessionMaterial($classSession, $attributes);
+            return $this->repository->createSessionMaterial(
+                $classSession,
+                $attributes,
+            );
         });
     }
 
-    public function deleteSessionMaterial(ClassSession $classSession, array $attributes)
-    {
+    public function deleteSessionMaterial(
+        ClassSession $classSession,
+        array $attributes,
+    ) {
         return DB::transaction(function () use ($classSession, $attributes) {
-            return $this->repository->deleteSessionMaterial($classSession, $attributes);
+            return $this->repository->deleteSessionMaterial(
+                $classSession,
+                $attributes,
+            );
         });
     }
-
 }
